793f64151a04a61801e3bcafedcd954ff48aeca4
Ryan Unruh <ryanunruhmedia@gmail.com>
2020-08-12 21:21:29 -0500

    Calculate results ecalculation functionality added

--- app.js
+++ app.js
@@ -6,16 +6,20 @@ document
 // Calculate results
 function calculateResults(e) {
   //  Ui Vars
-  const amount = document.getElementById("amount");
-  const interest = document.getElementById("interest"
-  const years = document.getElementById("years"
-  const monthlyPayment = document.getElementById("monthlyPayment"
-  const totalPayment = document.getElementById("totalPayment"
-  const totalInterest = document.getElementById("totalInterest"
+  const amount = document.getElementById("amount")
+  const interest = document.getElementById("interest")
+  const years = document.getElementById("years")
+  const monthlyPayment = document.getElementById("monthlyPayment")
+  const totalPayment = document.getElementById("totalPayment")
+  const totalInterest = document.getElementById("totalInterest")
 
   const principal = parseFloat(amount.value)
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
 
-  const principal = parseFloat(amount.value)
-  );
+  // Compute monthly payment
+  const x = Math.pow(1 +)
+  
+  
   e.preventDefault();
 }
